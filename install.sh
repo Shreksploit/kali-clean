7c6d73df3deb736efad5cc91e18354d5593ddd9c
Shreksploit <verbecque-tim@hotmail.com>
2025-06-17 18:55:46 +0200

    rlwrap and wsigdav

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt-get install autorecon -y
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update -y
-sudo apt-get install sublime-text -y
+sudo apt-get install sublime-text rlwrap -y
 
 #Alternative is to just set the wallpaper with this command
 #xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual1/workspace0/last-image -s images/kali-red-sticker-16x9.jpg
@@ -43,6 +43,7 @@ sudo apt-get install bloodyad -y
 #Either install certipy-ad or certipy
 sudo apt-get install python3-certipy -y
 pip install certipy-ad --break-system-package
+pip install wsgidav --break-system-packages
 
 sudo apt autoremove -y
 
